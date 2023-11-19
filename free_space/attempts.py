c3e360f07fe17deb0c99e2229028bcde9760c957
Anton Gavrilin <antgav@taltech.ee>
2023-11-19 12:03:01 +0200

    update

---
+++ free_space/attempts.py
@@ -0,0 +1,18 @@
+import re
+
+lines_to_write = []
+with open("data.txt", "r") as f:
+    lines = f.readlines()
+    count = 1
+    for line in lines:
+        line = line.strip()
+        line = re.sub(' +', ' ', line)
+        if count % 2 == 0:
+            lines_to_write[len(lines_to_write) - 1] = lines_to_write[len(lines_to_write) - 1] + " " + line + "\n"
+        else:
+            lines_to_write.append(line)
+        count += 1
+
+with open("new_data.txt", "w") as f:
+    for line_write in lines_to_write:
+        f.write(line_write)
